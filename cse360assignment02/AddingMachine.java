12bb6712307d65ab49311dc5feccf5cc6ca3a386
manaliRedkar <msredkar28@gmail.com>
2020-10-05 21:51:59 -0700

    Creating AddingMachine.java

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  //Contructor
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  //A function that returns the total value at any given point
+  public int getTotal () {
+    return 0;
+  }
+  
+  //Adds value and total
+  public void add (int value) {
+  }
+
+//Subtracts value and total 
+  public void subtract (int value) {
+  }
+
+//Returns the current string
+  public String toString () {
+    return "";
+  }
+
+//resets the  total and the string variable
+  public void clear() {
+  }
+}
